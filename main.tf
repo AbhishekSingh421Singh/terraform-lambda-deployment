d696edb4f49d7a12b3ec19ef6f09100091737c56
AbhishekSingh421Singh <73700379+AbhishekSingh421Singh@users.noreply.github.com>
2025-05-18 20:38:18 +0000

    Commiting the CI/CD pipeline

--- main.tf
+++ main.tf
@@ -1,20 +1,20 @@
 module "iam" {
-    source = "./modules/iam"
-    role_name = "lambda_exec_role"
+  source    = "./modules/iam"
+  role_name = "lambda_exec_role"
 }
 
 module "lambda" {
-    source = "./modules/lambda"
-    function_name = "Hello_World"
-    image_uri = "${var.account_id}.dkr.ecr.${var.region}.amazonaws.com/first:latest"
-    account_id = var.account_id
-    region     = var.region
+  source        = "./modules/lambda"
+  function_name = "Hello_World"
+  image_uri     = "${var.account_id}.dkr.ecr.${var.region}.amazonaws.com/first:latest"
+  account_id    = var.account_id
+  region        = var.region
 }
 
 module "apigateway" {
-    source = "./modules/api_gateway"
-    api_name = "Hello_World_test"
-    route_key = "GET /hello"
-    lambda_invoke_arn = module.lambda.invoke_arn
-    lambda_arn = module.lambda.arn
+  source            = "./modules/api_gateway"
+  api_name          = "Hello_World_test"
+  route_key         = "GET /hello"
+  lambda_invoke_arn = module.lambda.invoke_arn
+  lambda_arn        = module.lambda.arn
 }
