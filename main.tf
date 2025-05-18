15fafafa57feeefd4189591e9f5e1ae2ecf9b4b9
AbhishekSingh421Singh <73700379+AbhishekSingh421Singh@users.noreply.github.com>
2025-05-18 20:28:18 +0000

    Commiting the CI/CD pipeline

--- main.tf
+++ main.tf
@@ -7,7 +7,6 @@ module "lambda" {
     source = "./modules/lambda"
     function_name = "Hello_World"
     image_uri = "${var.account_id}.dkr.ecr.${var.region}.amazonaws.com/first:latest"
-    role_arn = module.iam.role_arn
     account_id = var.account_id
     region     = var.region
 
